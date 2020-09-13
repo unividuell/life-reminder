56291280991370cbab835e321049d661badd20ab
Clemens Hahn <clemens@couchbits.com>
2020-09-13 15:26:39 +0200

    list all calendars

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,15 @@
           Welcome to Agira
         </h1>
 
-        <p class="subheading font-weight-regular">Your upcoming TODOs:</p>
+        <p class="subheading font-weight-regular">Your calendars:</p>
+
+        <v-chip
+            class="ma-2"
+            color="primary"
+            v-for="calendar in calendars" :key="calendar.id"
+        >
+          {{ calendar.name }}
+        </v-chip>
 
       </v-col>
       <v-col cols="12" v-else>Please log in.</v-col>
@@ -15,20 +23,73 @@
 </template>
 
 <script>
+import GoogleCalendarApi from "@/api/GoogleCalendarApi";
+import { handlePromise } from "@/libs/common";
+
   export default {
     name: 'HelloWorld',
+    props: {
+      calendarId: {
+        type: String,
+        required: false,
+        default: "primary"
+      }
+    },
 
     data: () => ({
       currentUser: null,
+      googleCalendarApi: null,
+      calendars: null
     }),
-    created() {
+    async created() {
       if (this.isAuthenticated) {
         this.currentUser = this.$gapi.getUserData()
+        await this.initGoogleCalendarApi()
+        await this.retrieveCalendars()
       }
     },
     computed: {
       isAuthenticated() {
         return this.$store.state.authenticated
+      }
+    },
+    methods: {
+      async initGoogleCalendarApi() {
+        let response = await handlePromise(this.$gapi.getGapiClient());
+        if (response.success) {
+          const client = response.data.client;
+          this.googleCalendarApi = new GoogleCalendarApi(client);
+        } else {
+          // eslint-disable-next-line no-console
+          console.error("Failed to get gapi client", response.err);
+        }
+      },
+      async retrieveCalendars() {
+        const calendar = this.googleCalendarApi;
+        if (calendar) {
+          try {
+            const items = await calendar.retrieveCalendars();
+            let calendars = [];
+            // let primaryId = null;
+            items.forEach(item => {
+              let name = item.summary + (item.primary ? " (primary)" : "");
+              calendars.push({
+                id: item.id,
+                name,
+                primary: item.primary
+              });
+              // if (item.primary) primaryId = item.id;
+            });
+            // this.selectedCalendarId = this.calendarId || primaryId;
+            this.calendars = calendars;
+            console.log(calendars)
+            // If we do not have a calendarId we need to emit an event to retrieve the calendar events
+            // if (!this.calendarId) this.calendarChanged();
+          } catch (err) {
+            // eslint-disable-next-line no-console
+            console.error("Failed to retrieve calendars", err);
+          }
+        }
       }
     }
   }
