66dbd277ea1bb5df587d0b1dc277bcb4f279a04d
Clemens Hahn <clemens@couchbits.com>
2020-09-13 13:59:40 +0200

    install vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,93 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h3>Agira - no more noooi</h3>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
 
-    <button v-if="! isAuthenticated" @click="signIn">Sign In</button>
-    <button v-else @click="signOut">Sign Out</button>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Agira
+        </h1>
 
-    <template v-if="isAuthenticated">
-      <p>{{currentUser.email}}</p>
-      <p>{{currentUser.fullName}}</p>
-      <button @click="listEvents">List Events</button>
-    </template>
-  </div>
+        <p class="subheading font-weight-regular">Just do it!</p>
+
+        <v-btn v-if="! isAuthenticated" @click="signIn">Sign In</v-btn>
+        <v-btn v-else @click="signOut">Sign Out</v-btn>
+
+        <template v-if="isAuthenticated">
+          <p>{{currentUser.email}}</p>
+          <p>{{currentUser.fullName}}</p>
+<!--          <button @click="listEvents">List Events</button>-->
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data: () => ({
-    currentUser: null,
-    isAuthenticated: false,
-  }),
-  methods: {
-    signIn() {
-      if (! this.$gapi.isAuthenticated()) {
-        this.$gapi
-            .login(
-                () => {
-                  this.isAuthenticated = true
-                  this.currentUser = this.$gapi.getUserData()
-                  console.log("User logged in:", this.currentUser)
-                },
-                (err) => {
-                  console.error(err)
-                })
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      currentUser: null,
+      isAuthenticated: false,
+    }),
+    methods: {
+      signIn() {
+        if (! this.$gapi.isAuthenticated()) {
+          this.$gapi
+              .login(
+                  () => {
+                    this.isAuthenticated = true
+                    this.currentUser = this.$gapi.getUserData()
+                    console.log("User logged in:", this.currentUser)
+                  },
+                  (err) => {
+                    console.error(err)
+                  })
+        }
+      },
+      signOut() {
+        if (this.$gapi.isAuthenticated()) {
+          this.$gapi.logout(
+              () => {
+                this.isAuthenticated = false
+                this.currentUser = null
+                console.log("User disconnected.")
+              },
+              (err) => {
+                console.error(err.message)
+              })
+        }
       }
     },
-    signOut() {
-      if (this.$gapi.isAuthenticated()) {
-        this.$gapi.logout(
-            () => {
-              this.isAuthenticated = false
-              this.currentUser = null
-              console.log("User disconnected.")
-            },
-            (err) => {
-              console.error(err.message)
-            })
+    created() {
+      this.isAuthenticated = this.$gapi.isAuthenticated()
+      if (this.isAuthenticated) {
+        this.currentUser = this.$gapi.getUserData()
       }
-    },
-    listEvents() {
-      // this.$gapi.request({
-      //   path: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
-      //   method: 'GET'
-      // }).then(json => {
-      //   json = JSON.stringify(json, undefined, 2);
-      //   console.log(json)
-      // })
-      // .catch(err => {
-      //   console.error(err.error)
-      // })
-    }
-  },
-  created() {
-    this.isAuthenticated = this.$gapi.isAuthenticated()
-    if (this.isAuthenticated) {
-      this.currentUser = this.$gapi.getUserData()
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./store"
 import "./plugins/vue-gapi";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  store: store,
+  vuetify,
+  store: store
 }).$mount('#app')
