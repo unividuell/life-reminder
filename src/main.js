556801969c78f488a32b551f3817bac3a606929f
Clemens Hahn <clemens@couchbits.com>
2020-09-13 11:24:43 +0200

    extract vue-gapi configuration to own file

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueGAPI from 'vue-gapi'
+import store from "./store"
+import "./plugins/vue-gapi";
 
 Vue.config.productionTip = false
 
-const apiConfig = {
-  apiKey: process.env.VUE_APP_GAPI_KEY,
-  clientId: process.env.VUE_APP_GAPI_CLIENT_ID,
-  scope: 'email profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.calendars',
-  discoveryDocs: [ 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest' ],
-  // works only with `ux_mode: "prompt"`
-  refreshToken: true,
-}
-Vue.use(VueGAPI, apiConfig)
-
 new Vue({
   render: h => h(App),
+  store: store,
 }).$mount('#app')
