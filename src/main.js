ba99c2b961d62ecec13640486016e30f5d935c03
Clemens Hahn <clemens@couchbits.com>
2020-09-13 09:44:51 +0200

    store keys in .env file

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import VueGoogleApi from 'vue-google-api'
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: process.env.VUE_APP_GAPI_KEY,
+  clientId: process.env.VUE_APP_GAPI_CLIENT_ID,
+  scope: 'email profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.calendars',
+  discoveryDocs: [ 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest' ]
+}
+Vue.use(VueGoogleApi, config)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
