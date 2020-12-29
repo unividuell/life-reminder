a4007262b5209f1b05f7ceede4380729d3a9d7e1
Clemens Hahn <clemens@couchbits.com>
2020-12-29 11:27:41 +0100

    redirect to https in production

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+if (process.env.NODE_ENV !== "development" && location.protocol !== "https:") {
+  // in production we need SSL-connections (b/c of google credentials settings (only https is allowed))
+  // kudos: https://stackoverflow.com/a/10036029/810944
+  location.protocol = "https:";
+}
+
 new Vue({
   render: h => h(App),
   vuetify,
