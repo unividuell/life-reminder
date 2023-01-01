e111553b37206e88aa81bfdea70b5af5994ec34c
Clemens Hahn <clemens@couchbits.com>
2023-01-01 14:13:23 +0100

    use a list

--- src/components/LifeEventsListView.vue
+++ src/components/LifeEventsListView.vue
@@ -1,69 +1,77 @@
 <template>
  <v-container>
-   <v-expansion-panels accordion multiple v-model="panel">
-    
-     <v-expansion-panel>
-       <v-expansion-panel-title>Next up</v-expansion-panel-title>
-       <v-expansion-panel-text>
-         <v-row dense>
-           <v-col
-               cols="12" xs="12" md="12"
-               v-for="event in nextMonthEvents"
-               :key="event.id">
-             <LifeEvent :event="event" v-bind:key="event.googleId" v-on:reload="reload" />
-           </v-col>
-         </v-row>
-       </v-expansion-panel-text>
-     </v-expansion-panel>
-     <v-expansion-panel>
-       <v-expansion-panel-title>The future</v-expansion-panel-title>
-       <v-expansion-panel-text>
-         <v-row dense>
-           <v-col
-               cols="12" xs="12" md="12"
-               v-for="event in futureEvents"
-               :key="event.id">
-             <LifeEvent :event="event" v-bind:key="event.googleId" v-on:reload="reload" />
-           </v-col>
-         </v-row>
-       </v-expansion-panel-text>
-     </v-expansion-panel>
-      <v-expansion-panel>
-       <v-expansion-panel-title>Past events</v-expansion-panel-title>
-       <v-expansion-panel-text>
-         <v-row dense>
-           <v-col
-               cols="12" xs="12" md="6"
-               v-for="event in pastEvents"
-               :key="event.id">
-             <LifeEvent :event="event" v-bind:key="event.googleId" v-on:reload="reload" />
-           </v-col>
-         </v-row>
-       </v-expansion-panel-text>
-     </v-expansion-panel>
-   </v-expansion-panels>
+   <v-card class="mx-auto">
+     <v-list>
+       <v-list-item
+           v-for="event in items"
+           :key="event.id"
+           color="primary"
+           :active="currentlyInRedZone(event)"
+       >
+         <template v-slot:prepend="{ isSelected }">
+           <v-list-item-action start>
+             <v-checkbox-btn @click="toggleEventState(event)" :model-value="event.closed"></v-checkbox-btn>
+           </v-list-item-action>
+         </template>
+
+         <v-list-item-title :class="event.closed ? 'text-decoration-line-through' : ''">{{event.title}}</v-list-item-title>
+
+         <v-list-item-subtitle>
+           Clear until {{ event.redZone.end.toLocaleDateString() }} (starting at {{ event.redZone.start.toLocaleDateString() }})
+         </v-list-item-subtitle>
+
+         <template v-slot:append>
+           <v-btn @click="editEvent(event)" icon="mdi-pencil" variant="text" />
+           <v-btn @click="deleteEvent(event)" icon="mdi-delete" variant="text" />
+         </template>
+       </v-list-item>
+     </v-list>
+   </v-card>
+
  </v-container>
 </template>
 
 <script>
 import LifeEvent from "@/components/LifeEvent.vue";
-import {mapState} from "pinia";
+import {mapActions, mapState} from "pinia";
 import {useGoogleCalendarStore} from "@/stores/GoogleCalendarStore";
+import {useDialogStore} from "../stores/DialogStore";
+import {isWithinInterval} from "date-fns";
 
 export default {
   name: "LifeEventsListView",
   components: {LifeEvent},
   data: () => ({
     now: new Date(),
-    panel: [1]
+    test: []
   }),
   computed: {
-    ...mapState(useGoogleCalendarStore, ['sortedEvents', 'pastEvents', 'nextMonthEvents', 'futureEvents'])
+    ...mapState(useGoogleCalendarStore, ['sortedEvents', 'pastEvents', 'nextMonthEvents', 'futureEvents']),
+    items() {
+      return this
+          .sortedEvents
+          .map(e => ({...e, isSelected: e.closed}))
+          .reverse()
+    }
   },
   methods: {
-    reload() {
-      this.$emit('reload')
-    }
+    currentlyInRedZone(event) {
+      return isWithinInterval(this.now, {start: event.redZone.start, end: event.redZone.end})
+    },
+    deleteEvent(event) {
+      this.handleEventDeletion(event)
+    },
+    toggleEventState(event) {
+      let desiredState = event.closed ? 're-open' : 'close'
+      this.setEventState(event, desiredState)
+    },
+    setEventState(event, desiredState) {
+      this.handleEventState(event, desiredState)
+    },
+    editEvent(event) {
+      this.handleEventEditing(event)
+    },
+    ...mapActions(useDialogStore, ['handleEventState', 'handleEventDeletion', 'handleEventEditing'])
   }
 }
 </script>
