cdb23b31376dde170df052f280d1d2357cc5f09a
Clemens Hahn <clemens@couchbits.com>
2023-03-05 12:08:55 +0100

    fix value access

--- src/components/GoogleOneTapLogin.vue
+++ src/components/GoogleOneTapLogin.vue
@@ -12,14 +12,14 @@ const googleAuthenticationStore = useGoogleAuthenticationStore()
 const {loginIsPossible, isAuthenticated, currentUser, userDidLogout} = storeToRefs(googleAuthenticationStore)
 
 async function authenticate() {
-  if (! isAuthenticated) {
+  if (! isAuthenticated.value) {
     console.info(`need to login by one-tap as currently not authenticated`)
     await googleAuthenticationStore.authenticate()
   }
 }
 
 watch(loginIsPossible, async (newValue) => {
-  if (newValue && ! isAuthenticated) {
+  if (newValue && ! isAuthenticated.value) {
     console.info(`both login clients are ready, currently not authenticated => start authentication`)
     await authenticate()
   }
